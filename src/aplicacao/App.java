4419138506e711ca5c7dc84eb5e1c5883bf6aca2
Fernando <euevcfn@gmail.com>
2024-04-19 15:14:35 -0300

    Classe Departamento, entidades

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -1,5 +1,10 @@
+package aplicacao;
+
+import entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
