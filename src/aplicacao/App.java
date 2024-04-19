040bc62001a5eaf28ba6f4e96fdcb67c7617bb5c
Fernando <euevcfn@gmail.com>
2024-04-19 15:33:14 -0300

    Classe Seller criada no pacote Entities

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -1,10 +1,15 @@
 package aplicacao;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
